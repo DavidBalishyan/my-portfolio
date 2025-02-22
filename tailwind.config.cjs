6fa4730fc103c31f16f06ad62486a29afd8a1033
DavidBalishyan <davidbalishyan12@gmail.com>
2025-02-22 19:41:00 +0400

    use personalised logo instead of Vites one

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // Adjust paths to your project structure
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
